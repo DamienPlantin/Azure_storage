9d5e643ea3045ea3c8131abd92d46fe41ce2bf99
DamienPlantin <damien2809@gmail.com>
2021-03-12 11:44:25 +0100

    PEP8 + docstrings

--- main.py
+++ main.py
@@ -3,65 +3,98 @@ import argparse
 import configparser
 import logging
 import os.path
-from azure.storage.blob import BlobServiceClient, ContainerClient, BlobClient
+from azure.storage.blob import BlobServiceClient
 
 
 def listb(args, containerclient):
-    blob_list=containerclient.list_blobs()
+    """
+    Liste le contenu du blob
+    cmd : python main.py list
+    """
+    blob_list = containerclient.list_blobs()
     for blob in blob_list:
         print(blob.name)
 
 
 def upload(cible, blobclient):
+    """
+    Permet d'envoyer un ou plusieurs fichiers vers le blob en spécifiant
+    le chemin d'accès du ou des fichiers
+    cmd : python main.py upload 'chemin d'accès avec nom du/des fichier'
+    """
     with open(cible, "rb") as f:
         blobclient.upload_blob(f)
 
 
 def download(filename, dl_folder, blobclient):
-    with open(os.path.join(dl_folder,filename), "wb") as my_blob:
-        blob_data=blobclient.download_blob()
+    """
+    Permet de télécharger le contenu du container vers le dossier de restore
+    Il faut préciser le ou les fichiers à télécharger
+    cmd : python main.py download 'nom du/des fichiers'
+    """
+    with open(os.path.join(dl_folder, filename), "wb") as my_blob:
+        blob_data = blobclient.download_blob()
         blob_data.readinto(my_blob)
 
 
-def main(args,config):
-    blobclient=BlobServiceClient(
+def main(args, config):
+    """
+    Cette fonction est lancé au démarrage du script
+    Fais le liens avec le fichier config.ini
+    En fonction de l'argument, appel la fonction associée
+    """
+    blobclient = BlobServiceClient(
         f"https://{config['storage']['account']}.blob.core.windows.net",
         config["storage"]["key"],
         logging_enable=False)
-    containerclient=blobclient.get_container_client(config["storage"]["container"])
-    if args.action=="list":
+    containerclient = blobclient.get_container_client(
+        config["storage"]["container"])
+    if args.action == "list":
         return listb(args, containerclient)
     else:
-        if args.action=="upload":
-            blobclient=containerclient.get_blob_client(os.path.basename(args.cible))
+        if args.action == "upload":
+            blobclient = containerclient.get_blob_client(
+                os.path.basename(args.cible))
             return upload(args.cible, blobclient)
-        elif args.action=="download":
-            blobclient=containerclient.get_blob_client(os.path.basename(args.remote))
-            return download(args.remote, config["general"]["restoredir"], blobclient)
-    
+        elif args.action == "download":
+            blobclient = containerclient.get_blob_client(
+                os.path.basename(args.remote))
+            return download(
+                args.remote, config["general"]["restoredir"], blobclient)
 
-if __name__=="__main__":
-    parser=argparse.ArgumentParser("Logiciel d'archivage de documents")
-    parser.add_argument("-cfg",default="config.ini",help="chemin du fichier de configuration")
-    parser.add_argument("-lvl",default="info",help="niveau de log")
-    subparsers=parser.add_subparsers(dest="action",help="type d'operation")
-    subparsers.required=True
-    
-    parser_s=subparsers.add_parser("upload")
-    parser_s.add_argument("cible",help="fichier à envoyer")
 
-    parser_r=subparsers.add_parser("download")
-    parser_r.add_argument("remote",help="nom du fichier à télécharger")
-    parser_r=subparsers.add_parser("list")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Logiciel d'archivage de documents")
+    parser.add_argument(
+        "-cfg",
+        default="config.ini",
+        help="chemin du fichier de configuration")
+    parser.add_argument(
+        "-lvl", default="info", help="niveau de log")
+    subparsers = parser.add_subparsers(dest="action", help="type d'operation")
+    subparsers.required = True
 
-    args=parser.parse_args()
+    parser_s = subparsers.add_parser("upload")
+    parser_s.add_argument("cible", help="fichier à envoyer")
 
-    #erreur dans logging.warning : on a la fonction au lieu de l'entier
-    loglevels={"debug":logging.DEBUG, "info":logging.INFO, "warning":logging.WARNING, "error":logging.ERROR, "critical":logging.CRITICAL}
+    parser_r = subparsers.add_parser("download")
+    parser_r.add_argument("remote", help="nom du fichier à télécharger")
+    parser_r = subparsers.add_parser("list")
+
+    args = parser.parse_args()
+
+    # erreur dans logging.warning : on a la fonction au lieu de l'entier
+    loglevels = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL
+        }
     print(loglevels[args.lvl.lower()])
     logging.basicConfig(level=loglevels[args.lvl.lower()])
 
-    config=configparser.ConfigParser()
+    config = configparser.ConfigParser()
     config.read(args.cfg)
 
-    sys.exit(main(args,config))
+    sys.exit(main(args, config))
